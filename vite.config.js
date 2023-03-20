88ffed39bac7b7fc87b5c658f8ce104600c3dd6a
Fatima Ezzahraa El Madini <fatimzahraelmadini@gmail.com>
2023-03-20 09:29:00 +0100

    Add gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-base:"/maromatta/",
+base:"maromatta",
 build: {
          chunkSizeWarningLimit: 1600,
     },
